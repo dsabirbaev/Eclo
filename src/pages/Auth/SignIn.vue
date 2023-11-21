a716ce25ea860fd387e5c30e925bf746f7cc0a15
Davranbek <davran9559@gmail.com>
2023-11-22 02:00:01 +0500

    add login

--- src/pages/Auth/SignIn.vue
+++ src/pages/Auth/SignIn.vue
@@ -4,37 +4,54 @@
     <section class="bg-gray-50">
         <Toast />
         <div class="flex flex-col justify-center items-center h-screen w-screen">
-           
-            <router-link to="/"><img src="../../assets/icons/logo.svg" alt="logo"
-                    class="w-[300px] h-8 mr-2 object-cover object-center mb-2"> </router-link>
 
-            <div class="flex flex-col bg-white rounded-lg shadow p-4 w-[400px]">
-                <h2 class="text-center font-bold mb-4 text-[25px]">Sign in to your account</h2>
+           
+            <div class="flex flex-col bg-white rounded-lg shadow-2 p-4 w-[500px]">
+
+                <div class="text-center mb-5">
+                    <router-link to="/"><img src="../../assets/images/logo.jpg" alt="Image"  class="mb-1 bg-red-300 border h-[80px]" /> </router-link>    
+                    
+                    <div class="text-900 text-3xl font-medium mb-3">Welcome Eclo</div>
+                    <span class="text-600 font-medium ">Don't have an account?</span>
+                    <router-link to="/signup" class="font-medium  ml-2 text-blue-500 cursor-pointer">Create today!</router-link>
+                </div>
 
                 <form @submit="onSubmit" class="flex flex-col gap-y-4">
-                
+
                     <div class="flex flex-col relative">
-                        <InputMask v-model="phone" mask="99 999-99-99" placeholder="(99) 999-99-99"
+                        <label for="phone1" class="block text-900 font-medium mb-2">Phone</label>
+                        <InputMask id="phone1" v-model="phone" mask="99 999-99-99" placeholder="(99) 999-99-99"
                             :class="{ 'mb-0 p-invalid': phoneErrorMessage }" aria-describedby="mask-error" />
-                        <small class="p-error text-[10px] absolute w-full top-full" id="mask-error">{{ phoneErrorMessage ||
+                        <small class="p-error text-[12px] absolute w-full top-full" id="mask-error">{{ phoneErrorMessage ||
                             '&nbsp;' }}</small>
                     </div>
 
 
-
-                    <Password v-model="password" :feedback="false" inputStyleClass="w-full"  />
-
-                    <div class="flex items-center justify-between">
-                        <span></span> <span><a href="#" class="text-black text-[12px]">Forgot password?</a></span>
+                    <div class="flex flex-col relative">
+                        <label for="password1" class="block text-900 font-medium mb-2">Password</label>
+                        <InputText id="password1" v-model="password" type="password" placeholder="Password"
+                            :feedback="false" :class="{ 'p-invalid': passwordErrorMessage }"
+                            aria-describedby="text-error" />
+                        <small class="p-error text-[12px] absolute w-full top-full" id="text-error">{{ passwordErrorMessage
+                            || '&nbsp;' }}</small>
                     </div>
-                    <Button type="submit" label="Sign in" severity="success" />
+
+
+                    <div class="flex items-center justify-between mb-6">
+                        <div class="flex items-center">
+                            <Checkbox id="rememberme1" :binary="true" v-model="checked" class="mr-2"></Checkbox>
+                            <label for="rememberme1">Remember me</label>
+                        </div>
+                        <a class="font-medium  ml-2 text-blue-500 text-right cursor-pointer">Forgot
+                            password?</a>
+                    </div>
+
+
+                    <Button type="submit" label="Sign In" icon="pi pi-user" class="w-full"></Button>
                 </form>
 
-                <p className="text-sm font-light text-gray-500 mt-2">
-                    Don’t have an account yet?
-                    <router-link to="/signup" className="font-medium text-gray-700">Sign up</router-link>
-                </p>
-                <!-- <Toast /> -->
+               
+
             </div>
         </div>
     </section>
@@ -42,20 +59,20 @@
   
 <script setup>
 import InputMask from 'primevue/inputmask';
-import Password from 'primevue/password';
+import InputText from 'primevue/inputtext';
+import Checkbox from 'primevue/checkbox';
 import Button from 'primevue/button';
 
-import {useAuth} from "@sr/auth/useAuth.js";
+import { useAuth } from "@sr/auth/useAuth.js";
 import { useRouter } from "vue-router";
 import { useToast } from "primevue/usetoast";
 const toast = useToast();
-
 const { handleSubmit } = useForm();
 const router = useRouter();
 import { useField, useForm } from 'vee-validate';
 
 const { value: phone, errorMessage: phoneErrorMessage } = useField('phone', validatePhoneField);
-const { value: password } = useField('password', validatePasswordField);
+const { value: password, errorMessage: passwordErrorMessage } = useField('password', validatePasswordField);
 
 function validatePhoneField(value) {
     if (!value) {
@@ -66,43 +83,33 @@ function validatePhoneField(value) {
 }
 function validatePasswordField(value) {
     if (!value) {
-        return 'password is required.';
+        return 'Password is required.';
     }
 
     return true;
 }
 
 const onSubmit = handleSubmit((values) => {
-    
-    const user ={
+
+    const user = {
         phoneNumber: `+998${values.phone.replace(/\D/g, '')}`,
         password: values.password
     }
     console.log(user)
-    
-    // if (values.phone && values.phone.length > 0) {
-
-    //     // toast.add({ severity: 'info', summary: 'Form Submitted', detail: values.value, life: 3000 });
-    //     // resetForm();
-    // }
-
     useAuth.login(user).then((res) => {
         console.log(res)
         localStorage.setItem("token", res?.data?.token);
-      
-        // setTimeout(() => {
-        //   navigate("/");
-        // }, 1000)
-       
-        router.push({ name: "home" });
-        toast.add({ severity: 'success', summary: 'Login in', detail: 'Message Content', life: 3000 });
+
+        toast.add({ severity: 'success', detail: 'Logged in successfully!', life: 1000 });
+        setTimeout(() => {
+            router.push({ name: "home" });
+        }, 1000)
+
     }).catch((err) => {
-      
-    //   console.log(err.response.data)
+
+        toast.add({ severity: 'error', detail: 'An error occurred during login!', life: 2000 });
     })
 });
 </script>
   
-<style lang="scss" scoped>
-   
-</style>
+<style lang="scss" scoped></style>
