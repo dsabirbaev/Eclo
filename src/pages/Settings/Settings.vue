a58c114cb8cc3839d9b8a505e964aa7d59a797b0
Davranbek <davran9559@gmail.com>
2023-12-01 01:05:57 +0500

    fixed settings

--- src/pages/Settings/Settings.vue
+++ src/pages/Settings/Settings.vue
@@ -10,8 +10,8 @@
         <Toast />
         <div class="flex flex-col  items-center h-screen w-screen">
 
-            <div class="text-center  bg-white dark:bg-gray-800 dark:text-white w-[800px] mb-2 rounded-md py-4">
-                <div class="text-900 text-3xl font-medium">Profile</div>
+            <div class="p-4 bg-white dark:bg-gray-800 dark:text-white w-[800px] mb-2 rounded-md py-4">
+                <div class="text-900 text-3xl font-bold">Account Info</div>
             </div>
 
             <div class="flex flex-col bg-white dark:bg-gray-800 rounded-lg shadow-2 p-4 w-[800px]">
@@ -20,7 +20,8 @@
                 <form @submit.prevent="updateProfile" class="flex flex-col gap-y-4">
 
                     <div class="flex flex-col relative">
-                        <label for="name" class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">First
+                        <label for="name"
+                            class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">First
                             Name</label>
                         <InputText id="name" v-model.trim="user.firstName" type="text" class="w-full" required />
 
@@ -29,7 +30,8 @@
                         <div class="grow">
                             <div class="flex flex-col relative mb-2">
                                 <label for="surname"
-                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">Last Name</label>
+                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">Last
+                                    Name</label>
                                 <InputText id="surname" v-model.trim="user.lastName" type="text"
                                     class="w-full dark:text-white" required />
 
@@ -37,7 +39,7 @@
 
                             <div class="flex flex-col relative">
                                 <label for="passportseria"
-                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">Passport
+                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">Passport
                                     Serial Number</label>
                                 <InputText id="passportseria" v-model.trim="user.passportSerialNumber" type="text"
                                     class="w-full" required />
@@ -45,13 +47,17 @@
                             </div>
                         </div>
                         <div class="grow ">
-                            <p class="block text-900 font-medium mb-1 text-gray-600 mb-2 dark:text-white">Avatar</p>
+                            <p class="block text-900 font-medium mb-1 text-gray-600 mb-2 dark:text-white text-[14px]">Avatar
+                            </p>
                             <div class="flex items-center">
-                                <Avatar :image="`http://165.22.110.111:8080/${data.imagePath}`" class="mr-4 bg-slate-100"
-                                    size="xlarge" shape="circle" />
+                                <Avatar :image="`http://165.22.110.111:8080/${data.imagePath}`"
+                                    class="mr-4 bg-slate-100 object-cover" size="xlarge" shape="circle" />
 
-                                <input  type="file" mode="basic" name="demo[]" url="/api/image"
-                                    accept="image/*" :maxFileSize="1000000"  @change="uploadImage" class="w-[150px]" />
+                                <label for="upload" class="upload bg-green-600 rounded-md" >
+                                    <input id="upload" type="file" accept="image/*" @change="uploadImage" />
+                                    <i class="pi pi-upload mr-1"></i> <span class="font-semibold">Choose</span>
+                                </label>
+                               
                             </div>
                         </div>
                     </div>
@@ -59,7 +65,8 @@
                         <div class="grow">
                             <div class="flex flex-col relative mb-2">
                                 <label for="date"
-                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">Birth Date</label>
+                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">Birth
+                                    Date</label>
                                 <div class="flex items-center gap-x-1">
                                     <InputText :value="getDate" placeholder="Unknown" type="text" class="w-full" disabled />
                                     <InputText id="date" v-model.trim="user.birthDate" type="date" class="w-full"
@@ -71,20 +78,20 @@
 
                             <div class="flex flex-col relative">
                                 <label for="district"
-                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">District</label>
+                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">District</label>
                                 <InputText id="district" v-model.trim="user.district" type="text" class="w-full" required />
                             </div>
                         </div>
                         <div class="grow">
                             <div class="flex flex-col relative mb-2">
                                 <label for="region"
-                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">Region</label>
+                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">Region</label>
                                 <InputText id="region" v-model.trim="user.region" type="text" class="w-full" required />
                             </div>
 
                             <div class="flex flex-col relative">
                                 <label for="address"
-                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white">Address</label>
+                                    class="block text-900 font-medium mb-1 text-gray-600 dark:text-white text-[14px]">Address</label>
                                 <InputText id="address" v-model.trim="user.address" type="text" class="w-full" required />
 
                             </div>
@@ -96,7 +103,7 @@
 
                     </div>
 
-                    <Button type="submit" label="Update Profile" icon="pi pi-spinner"
+                    <Button type="submit" label="Update Profile" icon="pi pi-spin pi-spinner"
                         class="w-[200px] bg-green-700 border-green-700" />
 
 
@@ -115,7 +122,6 @@ import useUser from "@sr/user/useUser";
 import InputMask from 'primevue/inputmask';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-import FileUpload from 'primevue/fileupload';
 import Avatar from 'primevue/avatar';
 import { useToast } from "primevue/usetoast";
 
@@ -138,15 +144,14 @@ const user = reactive({
 
 const uploadImage = (e) => {
     const file = e.target.files[0];
-    // console.log(file)
-
+    console.log(file)
     user.imagePath = file;
 }
+
 
 const getProfile = async () => {
     try {
         const response = await useUser.profile();
-        // console.log(response.data);
         data.value = response.data;
         const date = new Date(response.data.birthDate);
         const day = ('0' + date.getDate()).slice(-2);
@@ -174,7 +179,7 @@ const updateProfile = () => {
     const day = ('0' + date.getDate()).slice(-2);
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const year = date.getFullYear();
-    const formatDate = `${year}-${month}-${day}`; 
+    const formatDate = `${year}-${month}-${day}`;
 
     const userData = {
         firstName: user.firstName,
@@ -188,11 +193,11 @@ const updateProfile = () => {
         phoneNumber: `+998${user.phoneNumber.replace(/\D/g, '')}`,
     }
 
-    console.log(userData)
-
     useUser.profileUpdate(userData).then((res) => {
-        console.log(res)
+
         toast.add({ severity: 'success', detail: 'Updated successfully!', life: 1500 });
+
+        getProfile();
     }).catch((err) => {
         console.log(err)
     })
@@ -203,4 +208,16 @@ onMounted(() => {
 })
 </script>
   
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input[type="file"] {
+    display: none;
+}
+
+.upload {
+    border: 1px solid transparent;
+    display: inline-block;
+    padding: 6px 20px;
+    cursor: pointer;
+    color: white;
+}
+</style>
